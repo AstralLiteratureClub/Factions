82d58774251a677a2494604a7f07a43e3ecca63b
Antritus <antt.graft@gmail.com>
2024-03-11 03:01:31 +0200

    @Cloud fix

--- src/main/java/bet/astral/unity/commands/join/JoinSubCommand.java
+++ src/main/java/bet/astral/unity/commands/join/JoinSubCommand.java
@@ -2,7 +2,14 @@ package bet.astral.unity.commands.join;
 
 
 import bet.astral.cloudplusplus.annotations.Cloud;
+import bet.astral.unity.Factions;
+import bet.astral.unity.commands.FactionCloudCommand;
+import org.bukkit.command.CommandSender;
+import org.incendo.cloud.paper.PaperCommandManager;
 
 @Cloud
-public class JoinSubCommand {
+public class JoinSubCommand extends FactionCloudCommand {
+	public JoinSubCommand(Factions plugin, PaperCommandManager<CommandSender> commandManager) {
+		super(plugin, commandManager);
+	}
 }
