cd93408aaac248d5d0cf727b64408b7198dd4a53
Antritus <antt.graft@gmail.com>
2024-03-13 15:44:49 +0200

    Update TranslationKey.java

--- src/main/java/bet/astral/unity/utils/TranslationKey.java
+++ src/main/java/bet/astral/unity/utils/TranslationKey.java
@@ -10,8 +10,8 @@ public final class TranslationKey {
 	public final static String DESCRIPTION_ROOT_FORCE = "root.description-force";
 	public final static String DESCRIPTION_ROOT_ALLY = "root.description-ally";
 	public final static String DESCRIPTION_ROOT_ALLY_FORCE = "root.description-ally";
-	public final static String DESCRIPTION_HELP = "help.description";
-	public final static String DESCRIPTION_HELP_QUERY = "help.description-query";
+	public final static String DESCRIPTION_HELP = "root.description-help";
+	public final static String DESCRIPTION_HELP_QUERY = "root.description-help-query";
 
 	public final static String DEFAULT_FACTION_DESCRIPTION = "faction-object.default-description";
 	public final static String DEFAULT_FACTION_JOIN_INFO = "faction-object.default-join-info";
@@ -121,107 +121,97 @@ public final class TranslationKey {
 
 	public final static String DESCRIPTION_LEAVE = "leave.description";
 	public final static String MESSAGE_LEAVE = "leave.message-leaving-player";
-	@Done
 	public final static String BROADCAST_LEAVE = "leave.broadcast-leaving-faction-of-leave";
 
 	public final static String DESCRIPTION_KICK = "kick.description";
-	public final static String DESCRIPTION_KICK_MEMBER = "kick.description-member";
-	public final static String DESCRIPTION_KICK_REASON = "kick.description-member";
-	public final static String MESSAGE_KICK = "kick.message-kicked";
-	public final static String BROADCAST_KICK = "kick.broadcast-kicked";
+	public final static String DESCRIPTION_KICK_MEMBER = "kick.description-member-arg";
+	public final static String DESCRIPTION_KICK_REASON = "kick.description-reason-arg";
+	public final static String MESSAGE_KICK_CANNOT_HIGHER = "kick.message-cannot-kick";
+	public final static String MESSAGE_KICK = "kick.message-kicked-of-kick";
+	public final static String BROADCAST_KICK = "kick.broadcast-faction-of-kick";
 
 	public final static String DESCRIPTION_FORCE_KICK = "kick.description-force";
-	public final static String DESCRIPTION_FORCE_KICK_FACTION = "kick.description-force";
-	public final static String DESCRIPTION_FORCE_KICK_PLAYER = "kick.description-force";
-	public final static String DESCRIPTION_FORCE_KICK_REASON = "kick.description-force";
-	public final static String MESSAGE_FORCE_KICK = "kick.message-kicked";
-	public final static String BROADCAST_FORCE_KICK = "kick.broadcast-kicked";
+	public final static String DESCRIPTION_FORCE_KICK_FACTION = "kick.description-force-faction-arg";
+	public final static String DESCRIPTION_FORCE_KICK_PLAYER = "kick.description-force-player-arg";
+	public final static String DESCRIPTION_FORCE_KICK_REASON = "kick.description-force-reason-arg";
+	public final static String MESSAGE_FORCE_KICK_SENDER = "kick.message-force-kick-success";
+	public final static String MESSAGE_FORCE_KICK_KICKED = "kick.message-force-kicked-about-kick";
+	public final static String BROADCAST_FORCE_KICK = "kick.broadcast-faction-about-force-kick";
 
 	public final static String DESCRIPTION_JOIN = "join.description";
-	public final static String DESCRIPTION_JOIN_FACTION = "join.description";
-	public final static String MESSAGE_JOINED = "join.message-join";
-	public final static String BROADCAST_JOINED = "join.broadcast-join";
+	public final static String DESCRIPTION_JOIN_FACTION = "join.description-faction-arg";
+	public final static String MESSAGE_FACTION_PRIVATE = "join.message-how-to-join";
+	public final static String MESSAGE_JOINED = "join.message-joined-public";
+	public final static String BROADCAST_JOINED = "join.broadcast-faction-about-new-member";
+	public final static String DESCRIPTION_FORCE_JOIN = "join.description-force";
+	public final static String DESCRIPTION_FORCE_JOIN_FACTION = "join.description-force";
+	public final static String DESCRIPTION_FORCE_JOIN_ROLE = "join.description-force";
+	public final static String MESSAGE_FORCE_JOIN_DEFAULT = "join.message-force-joined";
+	public final static String MESSAGE_FORCE_JOIN_NOT_DEFAULT = "join.broadcast-faction-of-force-join";
 
 	@IsCaption(true)
-	public final static Caption CAPTION_INVITE_ALREADY_HAS_FACTION = Caption.of("invite.");
+	public final static Caption CAPTION_UNKNOWN_ROLE = Caption.of("caption.unknown-role");
+	@IsCaption(true)
+	public final static Caption CAPTION_PRIVATE_FACTION = Caption.of("caption.private-faction");
+	@IsCaption(true)
+	public final static Caption CAPTION_UNKNOWN_MEMBER = Caption.of("caption.unknown-member");
+	@IsCaption(true)
+	public final static Caption CAPTION_NOT_INVITED = Caption.of("caption.player-has-not-been-invited");
+	@IsCaption(true)
+	public final static Caption CAPTION_ALREADY_INVITED = Caption.of("caption.player-has-already-been-invited");
 	@IsCaption(true)
 	public final static Caption CAPTION_HAS_A_FACTION = Caption.of("caption.has-a-faction");
 	@IsCaption(true)
 	public final static Caption CAPTION_HAS_NO_FACTION = Caption.of("caption.has-no-faction");
 	@IsCaption(true)
-	public final static Caption CAPTION_SELF_HAS_A_FACTION = Caption.of("caption.sender-has-a-faction");
+	public final static Caption CAPTION_CANNOT_CHECK_OFFLINE_PLAYER = Caption.of("caption.cannot-check-offline");
+	@IsCaption(true)
+	public final static Caption CAPTION_UNKNOWN_FACTION = Caption.of("caption.unknown-faction");
 	@IsCaption(true)
 	public final static Caption CAPTION_SELF_HAS_NO_FACTION = Caption.of("caption.sender-has-no-faction");
 	@IsCaption(true)
-	public final static Caption CAPTION_ALREADY_INVITED = Caption.of("caption.already-has-been-invited");
-	@IsCaption(true)
-	public final static Caption CAPTION_NOT_INVITED = Caption.of("caption.faction-has-not-been-invited");
-	@IsCaption(true)
-	public final static Caption CAPTION_HAS_NO_INVITES = Caption.of("caption.player-has-no-invites");
-	@IsCaption(true)
-	public final static Caption CAPTION_PLAYER_HAS_NOT_BEEN_INVITED = Caption.of("caption.player-has-not-been-invited");
-	@IsCaption(true)
-	public final static Caption CAPTION_UNKNOWN_INVITE = Caption.of("caption.faction-has-already-invited-player");
+	public final static Caption CAPTION_PLAYER_HAS_NOT_BEEN_INVITED = Caption.of("caption.faction-has-not-invited-player");
 	@IsCaption(true)
 	public final static Caption CAPTION_BANNED = Caption.of("caption.faction-has-banned-player");
-	@IsCaption(true)
-	public final static Caption CAPTION_FACTION = Caption.of("caption.unknown-faction");
-	@IsCaption(true)
-	public final static Caption CAPTION_CANNOT_CHECK_OFFLINE_PLAYER = Caption.of("caption.cannot-check-offline");
 
 
-
-	public final static String DESCRIPTION_META = "meta.description";
 	public final static String DESCRIPTION_PREFIX = "prefix.description";
-	public final static String DESCRIPTION_PREFIX_SET = "prefix.description-set";
-	public final static String DESCRIPTION_PREFIX_RESET = "prefix.description-reset";
+	public final static String DESCRIPTION_PREFIX_SET = "prefix.description-set-arg";
+	public final static String DESCRIPTION_PREFIX_RESET = "prefix.description-reset-arg";
 
-	public final static String DESCRIPTION_PREFIX_PUBLIC = "prefix.description";
-	public final static String DESCRIPTION_PREFIX_PUBLIC_ROLE = "prefix.description";
-//	public final static String DESCRIPTION_PREFIX_PUBLIC_MEMBER = "prefix.description";
-	public final static String DESCRIPTION_PREFIX_PUBLIC_PREFIX = "prefix.description";
-	public final static String MESSAGE_PREFIX_PUBLIC_TOO_SHORT = "prefix.too-short";
-	public final static String MESSAGE_PREFIX_PUBLIC_TOO_LONG = "prefix.too-long";
-	public final static String MESSAGE_PREFIX_PUBLIC_ILLEGAL = "prefix.illegal";
-	public final static String MESSAGE_PREFIX_PUBLIC_CHANGED = "prefix.changed";
+	public final static String DESCRIPTION_PREFIX_PUBLIC = "prefix.description-set-public-arg";
+	public final static String DESCRIPTION_PREFIX_PUBLIC_ROLE = "prefix.description-set-public-role-arg";
+	public final static String DESCRIPTION_PREFIX_PUBLIC_PREFIX = "prefix.description-set-public-prefix-arg";
+	public final static String DESCRIPTION_PREFIX_PRIVATE = "prefix.description-set-private-arg";
+	public final static String DESCRIPTION_PREFIX_PRIVATE_PLAYER = "prefix.description-set-private-player-arg";
+	public final static String DESCRIPTION_PREFIX_PRIVATE_PLAYER_MEMBER = "prefix.description-set-private-player-member-arg";
+	public final static String DESCRIPTION_PREFIX_PRIVATE_ROLE = "prefix.description-set-private-role-arg";
+	public final static String DESCRIPTION_PREFIX_PRIVATE_ROLE_ROLE = "prefix.description-set-private-role-role-arg";
+	public final static String DESCRIPTION_PREFIX_PRIVATE_PREFIX = "prefix.description-private-prefix-arg";
+	public final static String MESSAGE_PREFIX_PRIVATE_ROLE_CHANGED = "prefix.message-changed-private-role";
+	public final static String MESSAGE_PREFIX_PRIVATE_MEMBER_CHANGED = "prefix.message-changed-private-member";
+	public final static String MESSAGE_PREFIX_PUBLIC_TOO_SHORT = "prefix.message-public-prefix-too-short";
+	public final static String MESSAGE_PREFIX_PUBLIC_TOO_LONG = "prefix.message-public-prefix-too-long";
+	public final static String MESSAGE_PREFIX_PUBLIC_ILLEGAL = "prefix.message-public-prefix-illegal";
+	public final static String MESSAGE_PREFIX_PUBLIC_CHANGED = "prefix.message-changed-public-prefix";
 
-	public final static String DESCRIPTION_PREFIX_PRIVATE = "prefix.description";
-	public final static String DESCRIPTION_PREFIX_PRIVATE_PLAYER = "prefix.description";
-	public final static String DESCRIPTION_PREFIX_PRIVATE_ROLE = "prefix.description";
-	public final static String DESCRIPTION_PREFIX_PRIVATE_ROLE_ROLE = "prefix.description";
-	public final static String DESCRIPTION_PREFIX_PRIVATE_ROLE_PREFIX = "prefix.description";
-	public final static String MESSAGE_PREFIX_PRIVATE_ROLE_CHANGED = "prefix.changed";
 
-	public final static String DESCRIPTION_PREFIX_PRIVATE_MEMBER = "prefix.description";
-	public final static String DESCRIPTION_PREFIX_PRIVATE_MEMBER_MEMBER = "prefix.description";
-	public final static String DESCRIPTION_PREFIX_PRIVATE_MEMBER_PREFIX = "prefix.description";
-	public final static String MESSAGE_PREFIX_PRIVATE_MEMBER_CHANGED = "prefix.changed";
-
-//	public final static String DESCRIPTION_PREFIX_PRIVATE = "prefix.description";
-
-	public final static String DESCRIPTION_FORCE_BAN = "faction-ban.force-description";
-	public final static String DESCRIPTION_FORCE_BAN_FACTION = "faction-ban.force-description";
-	public final static String DESCRIPTION_FORCE_BAN_FACTION_FACTION = "faction-ban.force-description";
-	public final static String DESCRIPTION_FORCE_BAN_FACTION_REASON = "faction-ban.force-description";
-	public final static String DESCRIPTION_FORCE_BAN_FACTION_SILENT = "faction-ban.force-description";
-	public final static String MESSAGE_FORCE_BAN_ALREADY_BANNED = "faction-ban.message-banned";
-	public final static String MESSAGE_FORCE_BAN_BANNED = "faction-ban.message-banned";
-	public final static String BROADCAST_FORCE_BAN_BANNED_STAFF = "faction-ban.message-banned";
-	public final static String BROADCAST_FORCE_BAN_BANNED_PUBLIC = "faction-ban.message-banned";
-	public final static String BROADCAST_FORCE_BAN_BANNED_FACTION = "faction-ban.message-banned";
-
-	public final static String DESCRIPTION_FORCE_JOIN = "join.description-force";
-	public final static String DESCRIPTION_FORCE_JOIN_FACTION = "join.description-force";
-	public final static String DESCRIPTION_FORCE_JOIN_ROLE = "join.description-force";
-	public final static String MESSAGE_FORCE_JOIN_DEFAULT = "join.message-force-joined";
-	public final static String MESSAGE_FORCE_JOIN_NOT_DEFAULT = "join.message-force-joined";
-
+	public final static String DESCRIPTION_FORCE_BAN = "ban.force-description";
+	public final static String DESCRIPTION_FORCE_BAN_FACTION = "ban.force-factions.force-faction.description";
+	public final static String DESCRIPTION_FORCE_BAN_FACTION_FACTION = "ban.force-factions.force-faction.description-faction-arg";
+	public final static String DESCRIPTION_FORCE_BAN_FACTION_REASON = "ban.force-factions.force-faction.description-reason-arg";
+	public final static String DESCRIPTION_FORCE_BAN_FACTION_SILENT = "ban.force-factions.force-faction.description-silent-arg";
+	public final static String MESSAGE_FORCE_BAN_ALREADY_BANNED = "ban.force-factions.force-faction.message-already-banned";
+	public final static String MESSAGE_FORCE_BAN_BANNED = "ban.force-factions.force-faction.message-banned";
+	public final static String BROADCAST_FORCE_BAN_BANNED_STAFF = "ban.force-factions.force-faction.broadcast-admins-of-ban";
+	public final static String BROADCAST_FORCE_BAN_BANNED_PUBLIC = "ban.force-factions.force-faction.broadcast-public-of-ban";
+	public final static String BROADCAST_FORCE_BAN_BANNED_FACTION = "ban.force-factions.force-faction.broadcast-faction-of-ban";
 
 	@Retention(RetentionPolicy.RUNTIME)
 	public @interface IsCaption {
 		boolean value() default false;
 	}
 
-	public @interface Done {
+	public @interface NotDone {
 	}
 }
